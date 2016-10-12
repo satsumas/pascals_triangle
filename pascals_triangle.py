fcb3669f3aff485b599ebe885656dc29cf09bb80
Kate Hodesdon <kate@hodesdon.com>
2016-10-12 11:17:53 +0100

    first commit

---
+++ pascals_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+"""
+Pascal's triangle
+
+     1
+    1  1
+   1  2  1
+  1  3  3  1
+ 1  4  6  4  1
+
+ A function that takes integer n as an argument 
+ and returns the nth row of Pascal's triangle
+ """
+
+def pascal(n):
+    nth_row = []
+    for j in range(1, n+1):
+        if j == 1:
+            nth_row.append(1)
+        elif j == n:
+            nth_row.append(1)
+        else:
+            nth_row.append(pascal(n-1)[j-2] + pascal(n-1)[j-1])
+    return nth_row
+
